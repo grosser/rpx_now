2f0b81fd2196a90e5e579c038619b771c8df87d4
grosser <grosser.michael@gmail.com>
2009-10-07 18:49:39 +0200

    removed backwards compatability: set api_key, or pass :apiKey= in options

--- lib/rpx_now.rb
+++ lib/rpx_now.rb
@@ -18,26 +18,22 @@ module RPXNow
   # - cleaned Hash
   # - complete/unclean response when block was given user_data{|response| ...; return hash }
   # - nil when token was invalid / data was not found
-  def user_data(token, *args)
-    api_key, version, options = extract_key_version_and_options!(args)
-    options = {:token=>token,:apiKey=>api_key}.merge options
-
+  def user_data(token, options={})
+    options = options.merge(:token => token)
     begin
-      data = secure_json_post("/api/v#{version}/auth_info", options)
+      data = secure_json_post("auth_info", options)
+      if block_given? then yield(data) else parse_user_data(data) end
     rescue ServerError
       return nil if $!.to_s=~/Data not found/
       raise
     end
-    if block_given? then yield(data) else parse_user_data(data) end
   end
 
   # set the users status
-  def set_status(identifier, status, *args)
-    api_key, version, options = extract_key_version_and_options!(args)
-    options = {:identifier => identifier, :status => status, :apiKey => api_key}.merge options
-
+  def set_status(identifier, status, options={})
+    options = options.merge(:identifier => identifier, :status => status)
     begin
-      data = secure_json_post("/api/v#{version}/set_status", options)
+      data = secure_json_post("set_status", options)
     rescue ServerError
       return nil if $!.to_s=~/Data not found/
       raise
@@ -45,37 +41,32 @@ module RPXNow
   end
 
   # maps an identifier to an primary-key (e.g. user.id)
-  def map(identifier, primary_key, *args)
-    api_key, version, options = extract_key_version_and_options!(args)
-    options = {:identifier=>identifier,:primaryKey=>primary_key,:apiKey=>api_key}.merge options
-    secure_json_post("/api/v#{version}/map", options)
+  def map(identifier, primary_key, options={})
+    options = options.merge(:identifier => identifier, :primaryKey => primary_key)
+    secure_json_post("map", options)
   end
 
   # un-maps an identifier to an primary-key (e.g. user.id)
-  def unmap(identifier, primary_key, *args)
-    api_key, version, options = extract_key_version_and_options!(args)
-    options = {:identifier=>identifier,:primaryKey=>primary_key,:apiKey=>api_key}.merge options
-    secure_json_post("/api/v#{version}/unmap", options)
+  def unmap(identifier, primary_key, options={})
+    options = options.merge(:identifier => identifier, :primaryKey => primary_key)
+    secure_json_post("unmap", options)
   end
 
   # returns an array of identifiers which are mapped to one of your primary-keys (e.g. user.id)
-  def mappings(primary_key, *args)
-    api_key, version, options = extract_key_version_and_options!(args)
-    options = {:primaryKey=>primary_key,:apiKey=>api_key}.merge options
-    data = secure_json_post("/api/v#{version}/mappings", options)
+  def mappings(primary_key, options={})
+    options = options.merge(:primaryKey => primary_key)
+    data = secure_json_post("mappings", options)
     data['identifiers']
   end
 
-  def all_mappings(*args)
-    api_key, version, options = extract_key_version_and_options!(args)
-    data = secure_json_post("/api/v#{version}/all_mappings", {:apiKey => api_key}.merge(options))
+  def all_mappings(options={})
+    data = secure_json_post("all_mappings", options)
     data['mappings']
   end
 
-  def contacts(identifier, *args)
-    api_key, version, options = extract_key_version_and_options!(args)
-    options = {:apiKey => api_key, :identifier=> identifier}.merge(options)
-    data = secure_json_post("/api/v#{version}/get_contacts", options)
+  def contacts(identifier, options={})
+    options = options.merge(:identifier => identifier)
+    data = secure_json_post("get_contacts", options)
     RPXNow::ContactsCollection.new(data['response'])
   end
   alias get_contacts contacts
@@ -134,32 +125,14 @@ EOF
 EOF
   end
 
-  def extract_key_version_and_options!(args)
-    key, options = extract_key_and_options(args)
-    version = extract_version! options
-    [key, version, options]
-  end
-
-  # [API_KEY,{options}] or
-  # [{options}] or
-  # []
-  def extract_key_and_options(args)
-    if args.length == 2
-      [args[0],args[1]]
-    elsif args.length==1
-      if args[0].is_a? Hash then [@api_key,args[0]] else [args[0],{}] end
-    else
-      raise "NO Api Key found!" unless @api_key
-      [@api_key,{}]
-    end
+  def secure_json_post(method, data)
+    version = extract_version! data
+    path = "/api/v#{version}/#{method}"
+    Request.post(path, {:apiKey => api_key}.merge(data))
   end
 
   def extract_version!(options)
     options.delete(:api_version) || api_version
-  end
-
-  def secure_json_post(path, data)
-    Request.post(path, data)
   end
 
   class ServerError < RuntimeError; end #backwards compatibility / catch all
--- spec/rpx_now_spec.rb
+++ spec/rpx_now_spec.rb
@@ -1,6 +1,11 @@
 require 'spec/spec_helper'
 
 describe RPXNow do
+  def fake_response(replace={})
+    body = {'stat' => 'ok'}.merge(replace)
+    mock({:code => "200", :body => body.to_json})
+  end
+
   describe :api_key= do
     it "stores the api key, so i do not have to supply everytime" do
       RPXNow.api_key='XX'
@@ -79,14 +84,19 @@ describe RPXNow do
 
   describe :user_data do
     before do
-      @response_body = %Q({"profile":{"verifiedEmail":"grosser.michael@googlemail.com","displayName":"Michael Grosser","preferredUsername":"grosser.michael","identifier":"https:\/\/www.google.com\/accounts\/o8\/id?id=AItOawmaOlyYezg_WfbgP_qjaUyHjmqZD9qNIVM","email":"grosser.michael@gmail.com"},"stat":"ok"})
+      @response_body = {
+        "profile" => {
+          "verifiedEmail" => "grosser.michael@googlemail.com",
+          "displayName" => "Michael Grosser",
+          "preferredUsername" => "grosser.michael",
+          "identifier" => "https:\/\/www.google.com\/accounts\/o8\/id?id=AItOawmaOlyYezg_WfbgP_qjaUyHjmqZD9qNIVM",
+          "email" => "grosser.michael@gmail.com"
+        }
+      }
+      @response = fake_response(@response_body)
       @fake_user_data = {'profile'=>{}}
     end
 
-    def fake_response
-      mock(:code=>"200",:body=>@response_body)
-    end
-    
     it "raises ApiError when used with an invalid token" do
       lambda{
         RPXNow.user_data('xxxx')
@@ -104,69 +114,52 @@ describe RPXNow do
         :identifier => 'https://www.google.com/accounts/o8/id?id=AItOawmaOlyYezg_WfbgP_qjaUyHjmqZD9qNIVM',
         :username   => 'grosser.michael',
       }
-      RPXNow::Request.should_receive(:request).and_return fake_response
+      RPXNow::Request.should_receive(:request).and_return @response
       RPXNow.user_data('').should == expected
     end
     
     it "adds a :id when primaryKey was returned" do
-      @response_body.sub!(%Q("verifiedEmail"), %Q("primaryKey":"2","verifiedEmail"))
-      RPXNow::Request.should_receive(:request).and_return fake_response
+      @response_body['profile']['primaryKey'] = "2"
+      response = fake_response(@response_body)
+      RPXNow::Request.should_receive(:request).and_return response
       RPXNow.user_data('')[:id].should == '2'
     end
 
     it "handles primaryKeys that are not numeric" do
-      @response_body.sub!(%Q("verifiedEmail"), %Q("primaryKey":"dbalatero","verifiedEmail"))
-      RPXNow::Request.should_receive(:request).and_return fake_response
+      @response_body['profile']['primaryKey'] = "dbalatero"
+      response = fake_response(@response_body)
+      RPXNow::Request.should_receive(:request).and_return response
       RPXNow.user_data('')[:id].should == 'dbalatero'
     end
     
     it "hands JSON response to supplied block" do
-      RPXNow::Request.should_receive(:request).and_return mock(:code=>'200',:body=>%Q({"x":"1","stat":"ok"}))
+      RPXNow::Request.should_receive(:request).and_return @response
       response = nil
       RPXNow.user_data(''){|data| response = data}
-      response.should == {"x" => "1", "stat" => "ok"}
+      response.delete('stat') # dunno why it happens, but is not important...
+      response.should == @response_body
     end
     
     it "returns what the supplied block returned" do
-      RPXNow::Request.should_receive(:request).and_return mock(:code=>'200',:body=>%Q({"x":"1","stat":"ok"}))
+      RPXNow::Request.should_receive(:request).and_return @response
       RPXNow.user_data(''){|data| "x"}.should == 'x'
     end
     
     it "can send additional parameters" do
       RPXNow::Request.should_receive(:request).with{|url,data|
         data[:extended].should == 'true'
-      }.and_return fake_response
+      }.and_return @response
       RPXNow.user_data('',:extended=>'true')
     end
 
-    it "works with api key as 2nd parameter (backwards compatibility)" do
-      RPXNow.should_receive(:secure_json_post).with('/api/v2/auth_info', :apiKey=>'THE KEY', :token=>'id').and_return @fake_user_data
-      RPXNow.user_data('id', 'THE KEY')
-      RPXNow.api_key.should == API_KEY
-    end
-
-    it "works with api key as 2nd parameter and options (backwards compatibility)" do
-      RPXNow.should_receive(:secure_json_post).with('/api/v2/auth_info', :apiKey=>'THE KEY', :extended=>'abc', :token=>'id' ).and_return @fake_user_data
-      RPXNow.user_data('id', 'THE KEY', :extended=>'abc')
-      RPXNow.api_key.should == API_KEY
-    end
-
-    it "works with api version as option (backwards compatibility)" do
-      RPXNow.should_receive(:secure_json_post).with('/api/v123/auth_info', :apiKey=>API_KEY, :token=>'id', :extended=>'abc').and_return @fake_user_data
+    it "works with api version as option" do
+      RPXNow::Request.should_receive(:request).with('/api/v123/auth_info', :apiKey=>API_KEY, :token=>'id', :extended=>'abc').and_return @response
       RPXNow.user_data('id', :extended=>'abc', :api_version=>123)
       RPXNow.api_version.should == API_VERSION
     end
   end
 
   describe :set_status do
-    before do
-      @response_body = %Q({ "stat": "ok" })
-    end
-
-    def fake_response
-      mock(:code=>"200",:body=>@response_body)
-    end
-
     it "parses JSON response to result hash" do
       RPXNow::Request.should_receive(:request).and_return fake_response
       RPXNow.set_status('identifier', 'Chillen...').should == {'stat' => 'ok'}
@@ -185,34 +178,35 @@ describe RPXNow do
 
   describe :contacts do
     it "finds all contacts" do
-      response = JSON.parse(File.read('spec/fixtures/get_contacts_response.json'))
-      RPXNow.should_receive(:secure_json_post).with('/api/v2/get_contacts',:identifier=>'xx', :apiKey=>API_KEY).and_return response
+      response = fake_response(JSON.parse(File.read('spec/fixtures/get_contacts_response.json')))
+      RPXNow::Request.should_receive(:request).with('/api/v2/get_contacts',:identifier=>'xx', :apiKey=>API_KEY).and_return response
       RPXNow.contacts('xx').size.should == 5
     end
   end
 
   describe :mappings do
     it "parses JSON response to unmap data" do
-      RPXNow::Request.should_receive(:request).and_return mock(:code=>'200',:body=>%Q({"stat":"ok", "identifiers": ["http://test.myopenid.com/"]}))
-      RPXNow.mappings(1, "x").should == ["http://test.myopenid.com/"]
+
+      RPXNow::Request.should_receive(:request).and_return fake_response("identifiers"  =>  ["http://test.myopenid.com/"])
+      RPXNow.mappings(1).should == ["http://test.myopenid.com/"]
     end
   end
 
   describe :map do
     it "adds a mapping" do
-      RPXNow::Request.should_receive(:request).and_return mock(:code=>'200',:body=>%Q({"stat":"ok"}))
-      RPXNow.map('http://test.myopenid.com',1, API_KEY)
+      RPXNow::Request.should_receive(:request).and_return fake_response
+      RPXNow.map('http://test.myopenid.com',1)
     end
   end
 
   describe :unmap do
     it "unmaps a indentifier" do
-      RPXNow::Request.should_receive(:request).and_return mock(:code=>'200',:body=>%Q({"stat":"ok"}))
-      RPXNow.unmap('http://test.myopenid.com', 1, "x")
+      RPXNow::Request.should_receive(:request).and_return fake_response
+      RPXNow.unmap('http://test.myopenid.com', 1)
     end
 
     it "can be called with a specific version" do
-      RPXNow.should_receive(:secure_json_post).with{|a,b|a == "/api/v300/unmap"}
+      RPXNow::Request.should_receive(:request).with{|a,b|a == "/api/v300/unmap"}.and_return fake_response
       RPXNow.unmap('http://test.myopenid.com', 1, :api_key=>'xxx', :api_version=>300)
     end
   end
